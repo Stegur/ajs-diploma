52305540d5912b8cdc165ba7eae5e24c1cbd8c20
Stegura Alex <stegura.alex@gmail.com>
2020-01-02 23:17:38 +0300

    3. Отрисовка поля

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
+
+import getField from './themes';
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
@@ -9,6 +12,7 @@ export default class GameController {
   init() {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+    this.gamePlay.drawUi(getField());
   }
 
   onCellClick(index) {
--- src/js/themes.js
+++ src/js/themes.js
@@ -1,8 +1,28 @@
-const themes = {
-  prairie: 'prairie',
-  desert: 'desert',
-  arctic: 'arctic',
-  mountain: 'mountain',
+import GameStateService from './GameStateService';
+
+const getField = () => {
+  const stateService = new GameStateService(localStorage);
+  const level = stateService.load() === null ? 1 : stateService.load().level;
+
+  const themes = {
+    prairie: 'prairie',
+    desert: 'desert',
+    arctic: 'arctic',
+    mountain: 'mountain',
+  };
+
+  switch (level) {
+    case 1:
+      return themes.prairie;
+    case 2:
+      return themes.desert;
+    case 3:
+      return themes.arctic;
+    case 4:
+      return themes.mountain;
+    default:
+      throw new Error('Some problem with levels in themes.js');
+  }
 };
 
-export default themes;
+export default getField;

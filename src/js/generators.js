5c98ee3abc8b03ce836a0ecb9d6ca76cf61c4846
Stegura Alex <stegura.alex@gmail.com>
2020-01-02 22:09:41 +0300

    fix eslint temporary errors

--- src/js/generators.js
+++ src/js/generators.js
@@ -5,10 +5,11 @@
  * @param maxLevel max character level
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
+// eslint-disable-next-line no-unused-vars
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
 }
-
+// eslint-disable-next-line no-unused-vars
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
 }

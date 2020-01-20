710ca0660899b5812fcd3b4b5462919e51a47515
Stegura Alex <stegura.alex@gmail.com>
2020-01-20 23:40:56 +0300

    7. Отрисовка команд

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -2,6 +2,19 @@
 /* eslint-disable class-methods-use-this */
 
 import getField from './themes';
+import { generateTeam } from './generators';
+import { getRandom, arrayToPoint } from './utils';
+
+import PositionedCharacter from './PositionedCharacter';
+
+import Bowman from './characters/Bowman';
+import Swordsman from './characters/Swordsman';
+import Magician from './characters/Magician';
+
+import Undead from './characters/Undead';
+import Vampire from './characters/Vampire';
+import Daemon from './characters/Daemon';
+
 
 export default class GameController {
   constructor(gamePlay, stateService) {
@@ -13,6 +26,24 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi(getField());
+
+    const playerTeam = generateTeam([Bowman, Swordsman], 1, 2)
+      .map((hero) => {
+        const x = getRandom(0, 7);
+        const y = getRandom(0, 1);
+        const position = arrayToPoint([x, y]);
+        return new PositionedCharacter(hero, position);
+      });
+
+    const computerTeam = generateTeam([Undead, Vampire, Daemon], 1, 2)
+      .map((hero) => {
+        const x = getRandom(0, 7);
+        const y = getRandom(6, 7);
+        const position = arrayToPoint([x, y]);
+        return new PositionedCharacter(hero, position);
+      });
+
+    this.gamePlay.redrawPositions([...playerTeam, ...computerTeam]);
   }
 
   onCellClick(index) {
